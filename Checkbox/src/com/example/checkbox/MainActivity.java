0d2c588cf9d7c10e9b0f8f5b21792f41a393bf89
momotazpanna <panna31cse@gmail.com>
2015-02-25 05:24:32 -0800

    version2

--- Checkbox/src/com/example/checkbox/MainActivity.java
+++ Checkbox/src/com/example/checkbox/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-   private CheckBox chk1, chk2, chk3;
+   private CheckBox chk1, chk2, chk3,chk4;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +21,45 @@ public class MainActivity extends Activity {
       addListenerOnCheck1();
       addListenerOnCheck2();
       addListenerOnCheck3();
+       addListenerOnCheck4();
    }
 
    
-   private void addListenerOnCheck1() {
+   private void addListenerOnCheck4() {
+	   chk1 = (CheckBox) findViewById(R.id.checkBox1);
+	      chk2 = (CheckBox) findViewById(R.id.checkBox2);
+	      chk3 = (CheckBox) findViewById(R.id.checkBox3);
+	      chk4 = (CheckBox) findViewById(R.id.checkBox4);
+	      chk2.setOnClickListener(new OnClickListener() {
+
+	        @Override
+	        public void onClick(View v) {
+
+	          StringBuffer result = new StringBuffer();
+	          result.append("Java Selection : ").append
+	          (chk1.isChecked());
+	          result.append("\nPerl Selection : ").append
+	          (chk2.isChecked());
+	          result.append("\nPython Selection :").append
+	          (chk3.isChecked());
+				
+	          result.append("\nC Selection :").append
+	          (chk4.isChecked());
+				
+			Toast.makeText(MainActivity.this, result.toString(), 
+	           Toast.LENGTH_LONG).show();
+	        }
+	     });
+	// TODO Auto-generated method stub
+	
+}
+
+
+private void addListenerOnCheck1() {
       chk1 = (CheckBox) findViewById(R.id.checkBox1);
       chk2 = (CheckBox) findViewById(R.id.checkBox2);
       chk3 = (CheckBox) findViewById(R.id.checkBox3);
+      chk4 = (CheckBox) findViewById(R.id.checkBox4);
       chk2.setOnClickListener(new OnClickListener() {
 
         @Override
@@ -40,6 +72,9 @@ public class MainActivity extends Activity {
           (chk2.isChecked());
           result.append("\nPython Selection :").append
           (chk3.isChecked());
+			
+          result.append("\nC Selection :").append
+          (chk4.isChecked());
 			
 		Toast.makeText(MainActivity.this, result.toString(), 
            Toast.LENGTH_LONG).show();
@@ -66,6 +101,8 @@ public class MainActivity extends Activity {
              (chk2.isChecked());
              result.append("\nPython Selection :").append
              (chk3.isChecked());
+             result.append("\nC Selection :").append
+             (chk4.isChecked());
 
              Toast.makeText(MainActivity.this, result.toString  
              (),Toast.LENGTH_LONG).show();
@@ -79,6 +116,7 @@ public class MainActivity extends Activity {
 	    chk1 = (CheckBox) findViewById(R.id.checkBox1);
 	    chk2 = (CheckBox) findViewById(R.id.checkBox2);
 	    chk3 =  (CheckBox) findViewById(R.id.checkBox3);
+	    chk4 =  (CheckBox) findViewById(R.id.checkBox4); 
 	    
 		chk1.setOnClickListener(new OnClickListener() {
 		
@@ -89,7 +127,7 @@ public class MainActivity extends Activity {
 			 result.append("Java Selection : ").append(chk1.isChecked());
 			 result.append("\nPerl Selection : ").append(chk2.isChecked());
 			 result.append("\nPython Selection :").append(chk3.isChecked());
-			
+			 result.append("\nC Selection :").append(chk4.isChecked());
 			 Toast.makeText(MainActivity.this, result.toString(), Toast.LENGTH_LONG).show();
 			 
 		   }
